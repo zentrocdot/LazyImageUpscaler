ffdfd25e0b18db1307453ef37c6864675d82a01e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-11-13 15:36:43 +0100

    Add files via upload

---
+++ start_webui.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+#
+# Start Script
+# Version 0.0.0.1
+
+# Check if script is executed as sudo.
+function check_sudo {
+    if [ "$EUID" -eq 0 ]; then
+        # Print error message.
+        echo -e "Run script NOT as sudo!"
+        # Exit the script.
+        exit 1
+    fi
+}
+check_sudo
+
+# -------------------------------
+# Function make script executable
+# -------------------------------
+function make_executable {
+    # Assign the function argument to the local variable.
+    scriptname=$1
+    # Make the script executable.
+    if [[ "$(stat -c '%A' $0)" == *'x'* ]] ; then
+        echo -e "Script is executable!\n"
+    else
+        echo -e "Script is NOT executable yet!\n"
+        sudo chmod u+x "${scriptname}"
+    fi
+    # Return the error code 0.
+    return 0
+}
+make_executable
+
+# Start the Lazy Image Upscaler.
+python3 ./scripts/lazy_image_upscaler.py
+
+# Exit script.
+exit 0
