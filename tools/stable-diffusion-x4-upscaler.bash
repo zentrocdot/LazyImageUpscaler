a28ea0d3560471318b1cb61a9076dacdcc49a37f
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-11-22 14:07:47 +0100

    Add files via upload

---
+++ tools/stable-diffusion-x4-upscaler.bash
@@ -0,0 +1,16 @@
+#!usr/bin/bash
+#
+# Copy this file in the folder diffusion models amd then run it.
+#
+# Afterwords one needs to exchange the pointers by the real file.
+
+# Make sure you have git-lfs installed (https://git-lfs.com)
+git lfs install
+
+# Clone the repository.
+# git clone https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler
+
+# If you want to clone without large files - just their pointers
+GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler
+
+exit 0
